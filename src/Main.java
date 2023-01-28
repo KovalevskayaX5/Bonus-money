bf864d3b5e186a7db01f760ebcc52e41bc17c551
YanaK <Yana.Koval15@yandex.ru>
2023-01-28 20:23:20 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int w = 500; //начальный счет
-        int t = 1300;  //сумма пополнения
+        int account = 500; //начальный счет
+        int money = 1300;  //сумма пополнения
 
-        if (t > 1000) {
-            System.out.println(w + (t / 100));
+        if (money > 1000) {
+            System.out.println((account + (money / 100)) + "Баланс");
+            System.out.println((money/100) + "Бонус");
         } else {
 
 
-            if (t < 100) {
-                System.out.println(w + t);
+            if (money < 100) {
+                System.out.println((money + account) + "Баланс");
             }
 
         }
